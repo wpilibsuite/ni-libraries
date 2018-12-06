1d64efbcd9143d59f7dcaa0ad82cc816a93927ee
Thad House <thadhouse1@gmail.com>
2018-12-06 10:33:58 -0800

    Use updated UsageReporting file

--- src/include/FRC_NetworkCommunication/UsageReporting.h
+++ src/include/FRC_NetworkCommunication/UsageReporting.h
@@ -102,6 +102,7 @@ namespace nUsageReporting
         kResourceType_CAN,
         kResourceType_DigilentDMC60, // 80
         kResourceType_PWMVictorSPX,
+        kResourceType_RevSparkMax,
     } tResourceType;
 
     typedef enum
@@ -162,7 +163,7 @@ namespace nUsageReporting
 
     /**
      * Report the usage of a resource of interest.
-     * 
+     *
      * @param resource one of the values in the tResourceType above (max value 51).
      * @param instanceNumber an index that identifies the resource instance.
      * @param context an optional additional context number for some cases (such as module number).  Set to 0 to omit.
