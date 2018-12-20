47854805df81fee8a6b1eb29064bc4cc63e85330
Thad House <thadhouse1@gmail.com>
2018-12-20 11:10:13 -0800

    Update to image 12

--- src/include/FRC_NetworkCommunication/UsageReporting.h
+++ src/include/FRC_NetworkCommunication/UsageReporting.h
@@ -104,6 +104,7 @@ namespace nUsageReporting
         kResourceType_PWMVictorSPX,
         kResourceType_RevSparkMaxPWM,
         kResourceType_RevSparkMaxCAN,
+        kResourceType_ADIS16470,
     } tResourceType;
 
     typedef enum
@@ -164,7 +165,7 @@ namespace nUsageReporting
 
     /**
      * Report the usage of a resource of interest.
-     *
+     * 
      * @param resource one of the values in the tResourceType above (max value 51).
      * @param instanceNumber an index that identifies the resource instance.
      * @param context an optional additional context number for some cases (such as module number).  Set to 0 to omit.
